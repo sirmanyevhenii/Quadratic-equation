88e77021f5d79a678acd6d8a9a91223467632190
Yevhenii Sirman <sirmanj92@gmail.com>
2018-07-03 05:59:45 +0000

    fix js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,19 @@
-const a = parseInt(prompt('Enter number a'));
-const b = parseInt(prompt('Enter number b'));
-const c = parseInt(prompt('Enter number c'));
-const discr = (b * b) - (4 * a * c);
-let x1;
-let x2;
+const a = parseFloat(prompt('Enter number a'));
+const b = parseFloat(prompt('Enter number b'));
+const c = parseFloat(prompt('Enter number c'));
+const result = solveQuadEquation(a, b, c);
+alert(result);
 
-if (discr < 0) {
-    alert('Discriminant is ' + discr + '. No Roots');
-} else if(discr === 0) {
-    discrEqualZero();
-    alert('Discriminant is ' + discr + '. Roots x1 = x2 = ' + x1);
-} else if(discr > 0) {
-    discrOverZero();
-    alert('Discriminant is ' + discr + '. Root x1 = ' + x1 + '. Root x2 = ' + x2);
-}
-
-function discrEqualZero() {
-    return x1 = -(b / (2 * a));
-}
-
-function discrOverZero() {
-    x1 = ((-b) + Math.sqrt(discr)) / (2 * a);
-    x2 = ((-b) - Math.sqrt(discr)) / (2 * a);
-    return x1, x2;
+function solveQuadEquation(a, b, c) {
+    const discr = b**2 - 4 * a * c;
+    let x1, x2;
+    if(discr > 0) {
+        x1 = ((-b) + Math.sqrt(discr)) / (2 * a);
+        x2 = ((-b) - Math.sqrt(discr)) / (2 * a);
+        return 'Discriminant = ' + discr + '. Root x1 = ' + x1 + '. Root x2 = ' + x2;
+    } else if(discr === 0) {
+        x1 = -(b / (2 * a));
+        return 'Discriminant is ' + discr + '. Roots x1 = x2 = ' + x1;
+    }   
+    return 'Discriminant is ' + discr + '. No Roots';
 }
